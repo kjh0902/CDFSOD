62c57b2758dc276897b53ebe05d773b3f62336a1
Junhyung Kim <kimjunhyung0902@naver.com>
2026-07-11 12:15:56 +0900

    Prefer local mmdet package in training script

--- mmdetection/run_all_training.sh
+++ mmdetection/run_all_training.sh
@@ -6,6 +6,7 @@ SHOT="${2:-1}"
 GPUS="${3:-1}"
 MMDET_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$MMDET_DIR"
+export PYTHONPATH="$MMDET_DIR:${PYTHONPATH:-}"
 
 export CDFSOD_DATA_ROOT="${CDFSOD_DATA_ROOT:-/home/aislab5090/CDFSOD/junhyung/datasets}"
 export CDFSOD_DATASET="$DATASET"
